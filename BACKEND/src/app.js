84afd0aaec3172214cf173fb08bd85216bba69b4
Marcio.Costa <marciodeath@hotmail.com>
2020-04-30 10:02:56 -0300

    process.env.PORT - azure

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -5,7 +5,6 @@ const routes = require('./routes');
 
 const app = express();
 
-const port = 3333;
 
 app.use(cors());
 app.use(express.json());
--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -1,3 +1,5 @@
 const app = require('./app');
 
-app.listen(3333);
+const port = process.env.PORT || 3333;
+
+app.listen(port);
